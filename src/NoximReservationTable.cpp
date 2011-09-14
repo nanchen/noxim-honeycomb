4341f99f1412a63ef8c1d9a05fdeaf6157e30497
Nan Chen <mnanchen@gmail.com>
2011-09-14 16:27:57 +0300

    format NoximBuffer, modify ReservationTable

--- src/NoximReservationTable.cpp
+++ src/NoximReservationTable.cpp
@@ -10,66 +10,59 @@
 
 #include "NoximReservationTable.h"
 
-NoximReservationTable::NoximReservationTable()
-{
-    clear();
+NoximReservationTable::NoximReservationTable() {
+	clear();
 }
 
-void NoximReservationTable::clear()
-{
-    rtable.resize(DIRECTIONS + 1);
+void NoximReservationTable::clear() {
+	rtable.resize(DIRECTIONS_HM + 1);
 
-    // note that NOT_VALID entries should remain untouched
-    for (int i = 0; i < DIRECTIONS + 1; i++)
-	if (rtable[i] != NOT_VALID)
-	    rtable[i] = NOT_RESERVED;
+	// note that NOT_VALID entries should remain untouched
+	for (int i = 0; i < DIRECTIONS_HM + 1; i++)
+		if (rtable[i] != NOT_VALID)
+			rtable[i] = NOT_RESERVED;
 }
 
-bool NoximReservationTable::isAvailable(const int port_out) const
-{
-    assert(port_out >= 0 && port_out < DIRECTIONS + 1);
+bool NoximReservationTable::isAvailable(const int port_out) const {
+	assert(port_out >= 0 && port_out < DIRECTIONS_HM + 1);
 
-    return ((rtable[port_out] == NOT_RESERVED));
+	return ((rtable[port_out] == NOT_RESERVED));
 }
 
-void NoximReservationTable::reserve(const int port_in, const int port_out)
-{
-    // reservation of reserved/not valid ports is illegal. Correctness
-    // should be assured by NoximReservationTable users
-    assert(isAvailable(port_out));
+void NoximReservationTable::reserve(const int port_in, const int port_out) {
+	// reservation of reserved/not valid ports is illegal. Correctness
+	// should be assured by NoximReservationTable users
+	assert(isAvailable(port_out));
 
-    // check for previous reservation to be released
-    int port = getOutputPort(port_in);
+	// check for previous reservation to be released
+	int port = getOutputPort(port_in);
 
-    if (port != NOT_RESERVED)
-	release(port);
+	if (port != NOT_RESERVED)
+		release(port);
 
-    rtable[port_out] = port_in;
+	rtable[port_out] = port_in;
 }
 
-void NoximReservationTable::release(const int port_out)
-{
-    assert(port_out >= 0 && port_out < DIRECTIONS + 1);
-    // there is a valid reservation on port_out
-    assert(rtable[port_out] >= 0 && rtable[port_out] < DIRECTIONS + 1);
+void NoximReservationTable::release(const int port_out) {
+	assert(port_out >= 0 && port_out < DIRECTIONS_HM + 1);
+	// there is a valid reservation on port_out
+	assert(rtable[port_out] >= 0 && rtable[port_out] < DIRECTIONS_HM + 1);
 
-    rtable[port_out] = NOT_RESERVED;
+	rtable[port_out] = NOT_RESERVED;
 }
 
-int NoximReservationTable::getOutputPort(const int port_in) const
-{
-    assert(port_in >= 0 && port_in < DIRECTIONS + 1);
+int NoximReservationTable::getOutputPort(const int port_in) const {
+	assert(port_in >= 0 && port_in < DIRECTIONS_HM + 1);
 
-    for (int i = 0; i < DIRECTIONS + 1; i++)
-	if (rtable[i] == port_in)
-	    return i;		// port_in reserved outport i
+	for (int i = 0; i < DIRECTIONS_HM + 1; i++)
+		if (rtable[i] == port_in)
+			return i; // port_in reserved outport i
 
-    // semantic: port_in currently doesn't reserve any out port
-    return NOT_RESERVED;
+	// semantic: port_in currently doesn't reserve any out port
+	return NOT_RESERVED;
 }
 
 // makes port_out no longer available for reservation/release
-void NoximReservationTable::invalidate(const int port_out)
-{
-    rtable[port_out] = NOT_VALID;
+void NoximReservationTable::invalidate(const int port_out) {
+	rtable[port_out] = NOT_VALID;
 }
