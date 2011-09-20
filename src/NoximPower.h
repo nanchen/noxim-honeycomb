106ee0ddb8847beb4e8edfce4c6d4407cff3bda5
Nan Chen <mnanchen@gmail.com>
2011-09-20 23:30:25 +0300

    format NoximPower

--- src/NoximPower.h
+++ src/NoximPower.h
@@ -17,17 +17,17 @@ using namespace std;
 
 /*
 
-The average energy dissipated by a flit for a hop switch was estimated
-as being 0.151nJ, 0.178nJ, 0.182nJ and 0.189nJ for XY, Odd-Even, DyAD,
-and NoP-OE respectively
+ The average energy dissipated by a flit for a hop switch was estimated
+ as being 0.151nJ, 0.178nJ, 0.182nJ and 0.189nJ for XY, Odd-Even, DyAD,
+ and NoP-OE respectively
 
-We assumed the tile size to be 2mm x 2mm and that the tiles were
-arranged in a regular fashion on the floorplan. The load wire
-capacitance was set to 0.50fF per micron, so considering an average of
-25% switching activity the amount of energy consumed by a flit for a
-hop interconnect is 0.384nJ.
+ We assumed the tile size to be 2mm x 2mm and that the tiles were
+ arranged in a regular fashion on the floorplan. The load wire
+ capacitance was set to 0.50fF per micron, so considering an average of
+ 25% switching activity the amount of energy consumed by a flit for a
+ hop interconnect is 0.384nJ.
 
-*/
+ */
 
 #define PWR_ROUTING_XY             0.151e-9
 #define PWR_ROUTING_WEST_FIRST     0.155e-9
@@ -38,7 +38,6 @@ hop interconnect is 0.384nJ.
 #define PWR_ROUTING_FULLY_ADAPTIVE 0.0
 #define PWR_ROUTING_TABLE_BASED    0.185e-9
 #define PWR_ROUTING_MXPZ		   0.151e-9  //TODO how to determine the power of it?
-
 #define PWR_SEL_RANDOM             0.002e-9
 #define PWR_SEL_BUFFER_LEVEL       0.006e-9
 #define PWR_SEL_NOP                0.012e-9
@@ -49,43 +48,44 @@ hop interconnect is 0.384nJ.
 
 class NoximPower {
 
-  public:
+public:
 
-    NoximPower();
+	NoximPower();
 
-    void Routing();
-    void Selection();
-    void Standby();
-    void Forward();
-    void Incoming();
+	void Routing();
+	void Selection();
+	void Standby();
+	void Forward();
+	void Incoming();
 
-    double getPower() {
-	return pwr;
-    } double getPwrRouting() {
-	return pwr_routing;
-    }
-    double getPwrSelection() {
-	return pwr_selection;
-    }
-    double getPwrForward() {
-	return pwr_forward;
-    }
-    double getPwrStandBy() {
-	return pwr_standby;
-    }
-    double getPwrIncoming() {
-	return pwr_incoming;
-    }
+	double getPower() {
+		return pwr;
+	}
+	double getPwrRouting() {
+		return pwr_routing;
+	}
+	double getPwrSelection() {
+		return pwr_selection;
+	}
+	double getPwrForward() {
+		return pwr_forward;
+	}
+	double getPwrStandBy() {
+		return pwr_standby;
+	}
+	double getPwrIncoming() {
+		return pwr_incoming;
+	}
 
-  private:
+private:
 
-    double pwr_routing;
-    double pwr_selection;
-    double pwr_forward;
-    double pwr_standby;
-    double pwr_incoming;
+	double pwr_routing;
+	double pwr_selection;
+	double pwr_forward;
+	double pwr_standby;
+	double pwr_incoming;
 
-    double pwr;
+	double pwr;
 };
 
 #endif
