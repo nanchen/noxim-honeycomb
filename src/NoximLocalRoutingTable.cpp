0b7dd5ef35331bf2c1c1adafeae8d8d8a4b8f276
Nan Chen <mnanchen@gmail.com>
2011-09-21 22:40:08 +0300

    format for diff

--- src/NoximLocalRoutingTable.cpp
+++ src/NoximLocalRoutingTable.cpp
@@ -10,27 +10,23 @@
 
 #include "NoximLocalRoutingTable.h"
 
-NoximLocalRoutingTable::NoximLocalRoutingTable()
-{
+NoximLocalRoutingTable::NoximLocalRoutingTable() {
 }
 
 void NoximLocalRoutingTable::configure(NoximGlobalRoutingTable & rtable,
-				       const int _node_id)
-{
-    rt_node = rtable.getNodeRoutingTable(_node_id);
-    node_id = _node_id;
+		const int _node_id) {
+	rt_node = rtable.getNodeRoutingTable(_node_id);
+	node_id = _node_id;
 }
 
-NoximAdmissibleOutputs NoximLocalRoutingTable::
-getAdmissibleOutputs(const NoximLinkId & in_link, const int destination_id)
-{
-    return rt_node[in_link][destination_id];
+NoximAdmissibleOutputs NoximLocalRoutingTable::getAdmissibleOutputs(
+		const NoximLinkId & in_link, const int destination_id) {
+	return rt_node[in_link][destination_id];
 }
 
-NoximAdmissibleOutputs NoximLocalRoutingTable::
-getAdmissibleOutputs(const int in_direction, const int destination_id)
-{
-    NoximLinkId lid = direction2ILinkId(node_id, in_direction);
+NoximAdmissibleOutputs NoximLocalRoutingTable::getAdmissibleOutputs(
+		const int in_direction, const int destination_id) {
+	NoximLinkId lid = direction2ILinkId(node_id, in_direction);
 
-    return getAdmissibleOutputs(lid, destination_id);
+	return getAdmissibleOutputs(lid, destination_id);
 }
